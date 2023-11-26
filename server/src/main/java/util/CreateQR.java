96c72abeeddf5124bf672728419233763db50d4f
TMML <tml3200035928@outlook.com>
2023-11-26 20:36:24 +0800

    增加了生成二维码的代码文件

---
+++ server/src/main/java/util/CreateQR.java
@@ -0,0 +1,34 @@
+package util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class CreateQR {
+    /**
+     * 传入的是二维码要存储的信息
+     * @param text
+     * @param filePath
+     * @throws WriterException
+     * @throws IOException
+     */
+    public static void generateQRCodeImage(String text,String filePath)throws WriterException, IOException{
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE,100,100);
+        Path path = FileSystems.getDefault().getPath("D:\\STUDY\\1Android\\QR\\server\\src\\main\\resources\\"+filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix,"PNG",path);
+    }
+
+    public static void main(String[] args) throws IOException, WriterException {
+        generateQRCodeImage("what fuck!!!","nihao.png");
+    }
+
+}
